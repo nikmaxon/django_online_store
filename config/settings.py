de89f0f529a124d018a4dfd3aaabe10a775a18fb
nikmaxon <nikmakson99@gmail.com>
2023-07-06 17:43:27 +0300

    Изменения внесены, все css стили отображаются и работают

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 
 # Отвечает за формирование адреса доступа к статике
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+# Отвечает за место на диске, откуда необходимо подгружать статику
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
