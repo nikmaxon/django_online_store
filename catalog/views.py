80bbbc632b7a01af43c83c9ef0e08c59fd4bca30
nikmaxon <nikmakson99@gmail.com>
2023-07-06 17:59:50 +0300

    Попытка решить доп задание с формой - безуспешная

--- catalog/views.py
+++ catalog/views.py
@@ -7,4 +7,9 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST' or request.method == 'GET':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"{name} ({phone}): {message}")
     return render(request, 'catalog/contacts.html')
