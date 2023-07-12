76dd13a5fd85830c84508909d333016ada5071d3
nikmaxon <nikmakson99@gmail.com>
2023-07-12 11:39:50 +0300

    HW 3 добавлено очищение и добавление в fill

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -5,7 +5,11 @@ from catalog.models import Category, Product
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options) -> None:
+
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
         category_list = [
             {'name': 'Мясо', 'description': ''},
             {'name': 'Овощи', 'description': ''},
@@ -23,6 +27,7 @@ class Command(BaseCommand):
             {'product_name': 'Огурец', 'price': '350'},
         ]
         products_for_create = []
+
         for products_item in product_list:
             products_for_create.append(
                 Product(**products_item)
