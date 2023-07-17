88ae2d10c0e1f5412d79f391e085faade5af52da
nikmaxon <nikmakson99@gmail.com>
2023-07-17 16:30:59 +0300

    HW 3 внесены изменения по рекомендациям

--- catalog/models.py
+++ catalog/models.py
@@ -22,8 +22,8 @@ class Product(models.Model):
     preview_img = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='превью')
     category = models.ForeignKey(Category, **NULLABLE, on_delete=models.CASCADE, verbose_name="категория")
     price = models.IntegerField(verbose_name='цена')
-    creation_date = models.DateField(**NULLABLE, verbose_name='дата создания')
-    last_change_date = models.DateField(**NULLABLE, verbose_name='дата последнего изменения')
+    creation_date = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    last_change_date = models.DateField(auto_now=True, verbose_name='дата последнего изменения')
 
     def __str__(self):
         return f'{self.product_name} ({self.category}): {self.price}'
