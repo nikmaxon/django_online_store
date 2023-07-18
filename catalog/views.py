e8457cec1571dbc9992acc7d032b2bcf3e8f9986
nikmaxon <nikmakson99@gmail.com>
2023-07-18 13:06:05 +0300

    HW 4 часть сделана

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list,
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -12,4 +19,8 @@ def contacts(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f"{name} ({phone}): {message}")
-    return render(request, 'catalog/contacts.html')
+
+    context = {
+         'title': 'Контакты',
+    }
+    return render(request, 'catalog/contacts.html', context)
--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
+    path('', include('catalog.urls', namespace='catalog')),
 ]
 
 if settings.DEBUG:
