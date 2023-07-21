d58fd6de21ed58ab126a2dffc470357f0d1b4a19
nikmaxon <nikmakson99@gmail.com>
2023-07-21 15:12:08 +0300

    HW 4 добавлена возможность заходить в карточку

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_card
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('<int:pk>/card/', product_card, name='product_card'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -21,6 +21,16 @@ def contacts(request):
         print(f"{name} ({phone}): {message}")
 
     context = {
-         'title': 'Контакты',
+        'title': 'Контакты',
     }
     return render(request, 'catalog/contacts.html', context)
+
+
+def product_card(request, pk):
+    product_item = Product.objects.get(pk=pk)
+
+    context = {
+        'object_list': Product.objects.filter(pk=pk),
+        'title': f'Товар :{product_item.product_name}'
+    }
+    return render(request, 'catalog/card.html', context)
