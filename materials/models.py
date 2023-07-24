5a33dd6bb30e4da7263a2cf6e091f5b96a4bcb06
nikmaxon <nikmakson99@gmail.com>
2023-07-24 23:43:06 +0300

    HW 5 в работе. Реализовано новое приложение materials и методы view, list, edit, delete

--- materials/models.py
+++ materials/models.py
@@ -5,13 +5,13 @@ NULLABLE = {'null': True, 'blank': True}
 
 # Create your models here.
 class Material(models.Model):
-    title = models.CharField(max_length=150, verbose_name='заголовок')
-    slug = models.SlugField(max_length=100)
-    body = models.TextField(**NULLABLE, verbose_name='содержимое')
-    preview = models.ImageField(upload_to='materials/', **NULLABLE, verbose_name='превью')
+    title = models.CharField(max_length=300, verbose_name='заголовок')
+    slug = models.CharField(**NULLABLE, max_length=300)
+    body = models.TextField(verbose_name='содержимое')
+    preview = models.ImageField(**NULLABLE, upload_to='materials/',  verbose_name='превью')
     creation_date = models.DateField(auto_now_add=True, verbose_name='дата создания')
-    published = models.BooleanField(default=False, verbose_name='признак публикации')
-    views = models.IntegerField(default=0, verbose_name='количество просмотров')
+    published = models.BooleanField(**NULLABLE, default=False, verbose_name='признак публикации')
+    views = models.IntegerField(**NULLABLE, default=0, verbose_name='количество просмотров')
 
     def __str__(self):
         return self.title
--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, ListView, DetailView, UpdateView
+
+from materials.models import Material
+
 
 # Create your views here.
+class MaterialCreateView(CreateView):
+    model = Material
+    fields = ('title', 'body',)
+    success_url = reverse_lazy('materials:list')
+
+
+class MaterialDeleteView(DeleteView):
+    model = Material
+    success_url = reverse_lazy('materials:list')
+
+
+class MaterialUpdateView(UpdateView):
+    model = Material
+    fields = ('title', 'body',)
+    success_url = reverse_lazy('materials:list')
+
+
+class MaterialListView(ListView):
+    model = Material
+
+
+class MaterialDetailView(DetailView):
+    model = Material
+    template_name = 'materials/material_detail.html'
