1ff8f732e9b52db448d39972d0477bfc3e815d7c
nikmaxon <nikmakson99@gmail.com>
2023-08-10 17:08:09 +0300

    HW 7 готова

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, generate_new_password, activate_account, ActivationOk, \
-    ActivationFailed
+from users.views import RegisterView, ProfileView, generate_new_password, activate_account, ActivationOk, ActivationFailed
 
 app_name = UsersConfig.name
 
